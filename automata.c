b532b476061fd54a3e3102680f18825eee8d3b10
scarlettkuro <spider.kom@gmail.com>
2015-02-09 04:30:48 +0200

    char-automata good avalanch a new automata rule

--- automata.c
+++ automata.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <time.h>
-#define CONT 70
+#define CONT 5
+#define longway 20
 
-int a[CONT];
-int b[CONT];
-int c[CONT];
+typedef  char item;
 
-int *_next = &a;
-int *_cur = &b;
-int *_pre = &c;
+item a[CONT];
+item b[CONT];
+item c[CONT];
+
+item *_next = &a;
+item *_cur = &b;
+item *_pre = &c;
 
 void init();
 void step();
 void swip();
 void display();
-int local(int i);
+item local(int i);
 void reverse();
 
 int main() {
     int i,j;
     init();
-   // for(j=0; j<1; j++) {
-//    printf("%d\n",!0);
-    for(i = 0; i < 4; i++)
+    for(i = 0; i < longway; i++)
           step(0);
-    //printf("\n");
     reverse();
-    for(i = 0; i <4-2; i++)
+    for(i = 0; i <longway-1; i++)
           step(0);
           
     printf("\n");
-          //}
     getch();
 }
 
 void init() { 
+     /*
     srand(time(0));
     
     int i;
     for(i = 0; i < CONT; i++)
-          *(_cur + i) = rand()%2;
+          *(_cur + i) = (item)rand()%2;
+          */
+        int i;  
+    item text[CONT];
+    scanf("%s",&text);
+    item *t = &text[0];
+    for(i = 0; i < CONT; i++)
+          *(_next + i) =  *(t + i) ;
+          swip();
+    printf("\n");
 }
 
 void step() {
@@ -56,19 +65,19 @@ void swip() {
      for(i = 0; i < CONT; i++) 
           *(_pre + i) = *(_cur + i) ;
           
-     int *temp = _cur;
+     item *temp = _cur;
     _cur = _next;
     _next = temp;
 }
 
-int local(int i) {
-    int *left,*right ;
-    int *self = (_cur + i);
+item local(int i) {
+    item *left,*right ;
+    item *self = (_cur + i);
     if (i<0) left = _cur + CONT; else left = _cur + i - 1;
     if (i>=CONT) right = _cur; else right = _cur + i + 1;
     
-    int pre =   *(_pre + i);
-     
+    item pre =   *(_pre + i);
+     /*
     if (*left && *self && *right)
        return pre ^ 1;
        else
@@ -79,6 +88,8 @@ int local(int i) {
              return pre ^ 1;
     else
         return pre ^ 0;
+        */
+        return pre ^ (*left ^ *self * *right) ;
 }
 
 void reverse() {
@@ -93,11 +104,14 @@ void reverse() {
 void display() {
     int i;
     char s;
-    for(i = 0; i < CONT; i++) {
-          if (!*(_cur + i)) s = '0';
-                    else s = '_';
-          printf("%c", s);
-          }/*
+    int count =0;
+    for(i = 0; i < CONT; i++) //{
+         // count += *(_cur+i);
+          //if (!*(_cur + i)) s = '0';
+                    //else s = '_';
+          printf("%c", *(_cur + i));
+         // }
+         /*
     printf("|");
     for(i = 0; i < CONT; i++) {
           if (!*(_pre + i)) s = '0';
